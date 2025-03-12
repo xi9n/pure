79ffe828eae7f2f9baf1af98ad94f7324e50dd6b
xi9n <102434170+xi9n@users.noreply.github.com>
2025-03-12 17:17:54 +0000

    Update wwdwdwd.lua

--- wwdwdwd.lua
+++ wwdwdwd.lua
@@ -89,7 +89,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
     ["Fields"] = {
         ['AimAssistField'] = { ['Size'] = (500) },
         ['SilentAimField'] = { ['Size'] = (100) },
-        ['SilentAimBoxField'] = { ['Visible'] = (false), ['Sync'] = (true), ['Width'] = (3), ['Height'] = (5) }
+        ['SilentAimBoxField'] = { ['Visible'] = (false), ['Sync'] = (true), ['Width'] = (4), ['Height'] = (6) }
     },
 }
 
