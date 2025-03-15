1c70629067d6b0a822add43b58db9f5ae3b4ac79
xi9n <102434170+xi9n@users.noreply.github.com>
2025-03-15 14:26:44 +0100

    Update 123456789.lua

--- 123456789.lua
+++ 123456789.lua
@@ -1,6 +1,6 @@
 shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​u​t​f​i​t​D​i​s​p​a​t​c​h​e​s​I​X​P​2"] = {
     ["Main"] = {
-        ['Loader'] = { ['Key'] = ('ENTER YOUR KEY HERE') },
+        ['Loader'] = { ['Key'] = ('0fTGeAYXlypItGbyToGinb1dmIqgNBIG') },
     },
     
     ["Universal"] = {
